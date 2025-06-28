f07deab680b83c5652d5fb744b7e163adc489db0
Valentina Beatini <valentina.beatini@cae.au.dk>
2025-06-28 18:37:29 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1745,7 +1745,7 @@ $"(|shear|={Math.Abs(totalShear):F3}, limit={shearLimit:F3})");
                 GeometryModel geometry = new GeometryModel();
 
                     // Load faces and geometry 
-                LoadAllData(@"C:\Users\vb\OneDrive - Aarhus universitet\Dokumenter 1\work research\54 ICSA\JOURNAL paper\analyses\/stepped_friction_0e2.txt"   //data_pseudoparallel_friction_0e4  data_cairo_friction_0e01
+                LoadAllData(@"C:\Users\vb\OneDrive - Aarhus universitet\Dokumenter 1\work research\54 ICSA\JOURNAL paper\analyses\/data_cairo_friction_0e48.txt"   //data_pseudoparallel_friction_0e4  data_cairo_friction_0e01
                 , geometry, data);
                 DisplayBVector(data, geometry);
                 ComputeFaceNormalsFromGeometry(geometry);
